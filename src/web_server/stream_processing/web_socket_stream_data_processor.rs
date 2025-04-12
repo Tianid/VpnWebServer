a8a62391e90dbec745272a22337b13e470f09fa9
Lim Denson <tianid73@gmail.com>
2025-04-13 01:23:41 +0300

    Refactoring

--- src/web_server/stream_processing/web_socket_stream_data_processor.rs
+++ src/web_server/stream_processing/web_socket_stream_data_processor.rs
@@ -1,10 +1,14 @@
 use std::net::TcpStream;
 
+use crate::core::core_state::CoreState;
 use crate::web_server::stream_processing::stream_data_processor::StreamDataProcessor;
 use crate::web_server::connection_state::ConnectionState;
+use crate::web_server::web_socket::message_transferring::parse_request;
+use crate::web_server::web_socket::message_transferring::ws_request::RequestType;
 use crate::web_server::web_socket::ser_deser::decode_message;
 use crate::web_server::web_socket::sender;
 use crate::logger;
+use crate::core;
 
 #[derive(Debug, Clone, Copy)]
 pub struct WebSocketStreamDataProcessor { }
@@ -19,9 +23,39 @@ impl StreamDataProcessor for WebSocketStreamDataProcessor {
     fn process(self, stream: &mut TcpStream, size: usize, data: &[u8]) -> ConnectionState {
         let message = decode_message(&data[..size]);
         logger::info(format!("Receive message : {}", message).as_str());
-        sender::send(stream, message.as_str());
-        // FIXME process message
-        // FIXME send message
+
+        if message == "PING" {
+            sender::send(stream, "PONG");
+            return ConnectionState::KeepALive
+        }
+        
+        let request = parse_request(message.clone());
+        if request.is_none() {
+            logger::info(format!("receive unsupported message: {}", message).as_str());
+            return ConnectionState::KeepALive
+        }
+        
+        match request.unwrap().request_type {
+            RequestType::Connect    => core::connect_sync(),
+            RequestType::Disconnect => core::disconnect_sync(),
+            RequestType::Restart    => core::restart_sync(),
+        }
+
+        let status = core::calculate_state_sync();
+        sender::send(stream, &self.get_strings_from_status(status));
+
         ConnectionState::KeepALive
+    }
+}
+
+impl WebSocketStreamDataProcessor {
+    fn get_strings_from_status(&self, status: CoreState) -> String {
+        match status {
+            CoreState::Connected        => String::from("connected"),
+            CoreState::Connecting       => String::from("connecting"),
+            CoreState::Disconnected     => String::from("disconnected"),
+            CoreState::Reconnecting     => String::from("reconnecting"),
+            CoreState::Disconnecting    => String::from("disconnecting"),
+        }
     }
 }
